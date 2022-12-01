039fe5a44403c577cb8049fb84a8e9c8f5cdba23
Victor Greavu <viktorens10mc@gmail.com>
2022-12-01 14:33:43 +0200

    adding speranta webpage presentation

---
+++ js/speranta_webpage-script.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/uaDQNux2VWAf3Wh3/scene.splinecode');
